9e41c7887f5ececcffded38319bb1a2211fbf496
MARCANAKI <augustmarc16@gmail.com>
2025-04-24 13:05:53 +0100

    Global Error

---
+++ app/(auth)/sign-up.jsx
@@ -0,0 +1,12 @@
+// app/(auth)/sign-up.jsx
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SignUp() {
+  return (
+    <View>
+      <Text>Sign Up Page</Text>
+    </View>
+  );
+}
